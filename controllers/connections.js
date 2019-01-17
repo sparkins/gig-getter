d7c8798d19e86e1fe0c100592696d89ceea4d698
Sparky <17860180+sparkins@users.noreply.github.com>
2019-01-16 19:02:09 -0800

    merging mo's code

--- controllers/connections.js
+++ controllers/connections.js
@@ -4,11 +4,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-<<<<<<< HEAD
   password: "password",
-=======
-  password: "root",
->>>>>>> 068ada1cf5fc63c6ada0e1d7b41e8907dd98075a
   database: "giggetter_db"
 });
 
