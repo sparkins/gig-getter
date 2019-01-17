068ada1cf5fc63c6ada0e1d7b41e8907dd98075a
farahatco <farahatco@hotmail.com>
2019-01-17 05:42:31 +0300

    mo first push

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,6 +5,8 @@ var path = require('path');
 // var mysql = require('mysql');
 var connection = require('../controllers/connections.js');
 
+var bcrypt = require('bcryptjs');
+
 // Initializes the connection variable to sync with a MySQL database
 // if (process.env.JAWSDB_URL) {
 //   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -51,24 +53,91 @@ module.exports = function (app) {
     });
   });
 
-}
 
 //route for business and category info ----added by Alyssa. Please join the users table as well if you can so I can acess any data from any table with this route
-app.get('/allinfo', function (req, res){
-  connection.query('SELECT * FROM businesses LEFT JOIN categories ON categories.categoryId=businesses.categoryId', function(error, results, fields){
+  app.get('/allinfo', function (req, res){
+   connection.query('SELECT * FROM businesses LEFT JOIN categories ON categories.categoryId=businesses.categoryId', function(error, results, fields){
     if(error) throw error;
-    res.json(results);
+     res.json(results);
   })
 })
 
 // Sign Up Form Data
-app.get('/signup', function(req, res){
-  connection.query("INSERT INTO users (username, email, password_hash, isABusiness", [req.query.username, req.query.email, req.query.pass, req.query.bus],
-  function(err, results, fields){
-    if(err) console.log('there was an error');
-    res.redirect('/start')
+  app.get('/signup', function (req, res) {
+    console.log(req.query.bus)
+    var bus = req.query.bus;
+     if (bus==="true")
+       bus=1;
+     else  
+      bus=0;
+  
+      bcrypt.genSalt(10, function (err, salt) {
+      bcrypt.hash(req.query.pass, salt, function (err, p_hash) {
+  
+      connection.query("INSERT INTO users (username, email, password_hash, isABusiness) values(?,?,?,?)", [req.query.username, req.query.email,p_hash,bus],function (err, results, fields) {
+            if (err) 
+            console.log('there was an error');
+            else
+            {
+              var Id = results.insertId; 
+              connection.query("select * from users where userid = (?)", [Id],function (err, rows , fields){ 
+                req.session.user_id = rows[0].userId;
+                req.session.email = rows[0].email;
+                req.session.username = rows[0].username;
+                //res.redirect('/start')
+                res.render("start",{user: req.session.username})
+            })           
+            }
+          })
+      })
+    })   
   })
-})
+
+  //                 **** check session before rendering page *****
+  /*                  if (req.session.user_id && req.cookies.user_sid) 
+                   {
+                       res.render("index", { user: req.session.username })
+                   }
+                     else 
+                   {
+                     res.render("logon")
+                   }
+
+  */
+
+
+  // Sign Up Form Data
+    app.get('/signin', function (req, res) {   
+      connection.query('SELECT * FROM users WHERE email = ?', [req.query.emaillogin], function (error, results, fields) {
+      if (error) 
+      throw error;
+      if (results.length == 0) 
+       {
+         res.send('try again');
+       }
+      else 
+       {
+       bcrypt.compare(req.query.passlogin, results[0].password_hash, function (err, result) {
+      if (result == true) 
+       {
+       req.session.user_id = results[0].id;
+       req.session.email = results[0].email;
+       req.session.username = results[0].username;
+       //res.redirect('/start')
+       res.render("start",{user: req.session.username})
+       }
+      else 
+       {
+      console.log(" ERROR ")
+      res.redirect('/');
+       }
+     });
+       }
+    })
+    }); 
+
+
+ 
 
 
 //log in form data
@@ -85,3 +154,4 @@ app.get("/createad", function(req, res){
   //insert into [NEED TO MAKE TABLE COLUMN FOR THIS DATA, PREFERABLY SAME TABLE AS THE JOB TITLE/CATEGORY] req.query.jobdescription
   //job description is a block of text rather than a short string, so should use TEXT or something other than VARCHAR in the table
 })
+}
