688d3dcc4cd6202c587e19cf1fb17acc3d43c664
Sparky <17860180+sparkins@users.noreply.github.com>
2019-01-31 14:31:32 -0800

    created css structure and move styles to new css files

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -9,9 +9,9 @@ app.use(express.static("public"));
 
 module.exports = function (app) {
 
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../public/home.html'))
-  });
+  // app.get('/', function (req, res) {
+  //   res.render(path.join(__dirname, '/../public/home.html'))
+  // });
 
   app.get('/home', function (req, res) {
     if (req.session.user_id && req.cookies.user_sid) {
